2b86340bf6b18575ef103b92368d1a071221e431
patrick <pconway@inhancetechnology.com>
2016-11-29 14:32:54 +0000

    Initial commit

---
+++ src/main/java/io/magpi/MainVerticle.java
@@ -0,0 +1,12 @@
+package io.magpi;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+
+
+    }
+}
