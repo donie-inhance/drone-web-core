d502866198237663973ad01a812ea81fbd5957c9
patrick <pconway@inhancetechnology.com>
2016-11-30 14:48:17 +0000

    ...

--- src/main/java/io/magpi/api/RestApi.java
+++ src/main/java/io/magpi/api/RestApi.java
@@ -28,6 +28,7 @@ public class RestApi  {
          */
         apiRouter.route(GET,"/elevation").handler(new ElevationHandler());
 
+
         /*
             Given lat , long and bearing calculate a grid
             and return grid as an array of gps coordinates and with elevations
